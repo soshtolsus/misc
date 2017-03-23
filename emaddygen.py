890764f0d0f22b13f6be99b04fac1f1f8f315589
cj4 <arhasza@gmail.com>
2017-03-23 00:45:12 -0500

    added --position argument

--- emaddygen.py
+++ emaddygen.py
@@ -8,13 +8,44 @@ import sys
 import argparse
 
 
+class Position:
+	CALC_POSITION = object()
+	FIRST_POSITION = object()
+	LAST_POSITION = object()
+	
+	CALC_CHOICES = ('c', 'calc', 'calculated')
+	FIRST_CHOICES = ('f', 'first')
+	LAST_CHOICES = ('l', 'last')
+
+	@classmethod
+	def parse(cls, value):
+		if value in cls.CALC_CHOICES:
+			return cls.CALC_POSITION
+		elif value in cls.FIRST_CHOICES:
+			return cls.FIRST_POSITION
+		elif value in cls.LAST_CHOICES:
+			return cls.LAST_POSITION
+		else:
+			raise Exception('how did you get here?')
+
+	@property
+	def choices(self):
+		return self.CALC_CHOICES + self.FIRST_CHOICES + self.LAST_CHOICES
+
+
 def parse_args():
 	parser = argparse.ArgumentParser(description=__doc__.strip('\n'), formatter_class=argparse.RawDescriptionHelpFormatter)
 	parser.add_argument('key', action='store', help='key with which to munge')
 	parser.add_argument('name', action='store', help='the legible part of the address')
-	parser.add_argument('--size', action='store', type=int, default=16, help='number of hash characters to keep')
+	parser.add_argument('--size', '-s', action='store', type=int, default=16, help='number of hash characters to keep')
+	parser.add_argument('--position', '-p', default='calculated', choices=Position().choices, help='where to put the legible part')
 	
 	return parser.parse_args()
+
+def process_args(args):
+	args.position = Position.parse(args.position)
+
+	return args
 
 def main(args):
 	hash_key = skein.skein1024(init=args.key.encode(), digest_bits=512+args.size, pers=b'emaddygen').digest()
@@ -26,9 +57,13 @@ def main(args):
 
 	hash_str = letterify(hash_int)[:args.size]
 
-	insert_position = hash_int % (args.size - 2) + 1
-
-	print('{}.{}.{}'.format(hash_str[:insert_position], args.name, hash_str[insert_position:]))
+	if args.position is Position.FIRST_POSITION:
+		print('.'.join((args.name, hash_str)))
+	elif args.position is Position.LAST_POSITION:
+		print('.'.join((hash_str, args.name)))
+	else:
+		insert_position = hash_int % (args.size - 2) + 1
+		print('.'.join((hash_str[:insert_position], args.name, hash_str[insert_position:])))
 
 def letterify(hash_int):
 	to_return = []
@@ -39,6 +74,6 @@ def letterify(hash_int):
 
 if __name__ == '__main__':
 	try:
-		main(parse_args())
+		main(process_args(parse_args()))
 	except KeyboardInterrupt:
 		print('exiting gracefully', file=sys.stderr)
